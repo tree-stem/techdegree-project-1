a0025f5bf282d131c00c15bdcd1778316abdf7d7
Tristan Ringer <rin9er@gmail.com>
2024-12-25 01:31:56 -0700

    Updated comments

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -3,76 +3,76 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-
-// created a quotes array to store object information 
+// Created a quotes array to store quotes and their information as objects
 
 const quotes = [
   {
-    quote: 'Fear is the mind-killer.',
-    source: 'Lady Jessica Atreides',
-    citation: 'Dune: Part One',
-    year: ''
-  },
-    {
-    quote: 'I will take the ring though I do not know the way.',
-    source: 'Frodo Baggins',
-    citation: 'Lord of the Rings: The Fellowship of the Ring',
-    year: ''
-  },
-    {
-    quote: 'Every man dies, not every man really lives.',
-    source: 'William Wallace',
-    citation: 'Braveheart',
-    year: ''
+    quote: "Fear is the mind-killer.",
+    source: "Lady Jessica Atreides",
+    citation: "Dune: Part One",
+    year: "",
   },
   {
-    quote: 'It is not in doing what you like, but in liking what you do that is the secret of happiness.',
-    source: 'Peter Pan',
-    citation: 'Peter Pan',
-    year: ''
+    quote: "I will take the ring though I do not know the way.",
+    source: "Frodo Baggins",
+    citation: "Lord of the Rings: The Fellowship of the Ring",
+    year: "",
   },
   {
-    quote: 'You will never truly know yourself, or the strength of your relationships, until both have been tested by adversity.',
-    source: 'J.K Rowling',
-    citation: 'J.K. Rowling',
-    year: '2008'
-  }
+    quote: "Every man dies, not every man really lives.",
+    source: "William Wallace",
+    citation: "Braveheart",
+    year: "",
+  },
+  {
+    quote:
+      "It is not in doing what you like, but in liking what you do that is the secret of happiness.",
+    source: "Peter Pan",
+    citation: "Peter Pan",
+    year: "",
+  },
+  {
+    quote:
+      "You will never truly know yourself, or the strength of your relationships, until both have been tested by adversity.",
+    source: "J.K Rowling",
+    citation: "J.K. Rowling",
+    year: "2008",
+  },
 ];
 
+// Created a function that generates a random number and returns a quote from the selected array
 
-// created a function to generate a random number to return one of the array's objects
-
-function getRandomQuote () {
-  let randomNumber = Math.floor( Math.random() * 5)
+function getRandomQuote() {
+  let randomNumber = Math.floor(Math.random() * 5);
   let randomObject = quotes[randomNumber];
   return randomObject;
-};
+}
 
+// Created a function that displays a different random quote and a random background color
 
-// created a function to display a random quote stored in the object's properties
-
-function printQuote () {
+function printQuote() {
   let randomQuote = getRandomQuote();
-  let html = 
-        `<p class = "quote">${randomQuote['quote']}</p>
-        <p class = "source">${randomQuote['source']}`;
+  let html = `<p class = "quote">${randomQuote["quote"]}</p>
+        <p class = "source">${randomQuote["source"]}`;
 
-  if (randomQuote['citation']) {
-      html += `<span class = "citation" >${randomQuote['citation']}</span>`;
-  };
-    
-  if (randomQuote['year']) {
-      html += `<span class = "year">${randomQuote['year']}</span>`;
-  };
+  if (randomQuote["citation"]) {
+    html += `<span class = "citation" >${randomQuote["citation"]}</span>`;
+  }
 
-  html += '</p>'
+  if (randomQuote["year"]) {
+    html += `<span class = "year">${randomQuote["year"]}</span>`;
+  }
 
-  document.getElementById('quote-box').innerHTML = html; 
-};
+  html += "</p>";
+
+  document.getElementById("quote-box").innerHTML = html;
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false);
